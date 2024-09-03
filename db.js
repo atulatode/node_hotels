557a1555062d9d82cecdccf77a21ec76b87d3300
Atul Atode <atul.atode@tekzee.com>
2024-09-03 11:49:08 +0530

    dotEnv

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 //Define the MongoDB connection URL
-const mongoURL = 'mongodb://localhost:27017/hotels';
+// const mongoURL = 'mongodb://localhost:27017/hotels';
+const mongoURL = process.env.MONGODB_URL;
 
 //Establish conncection
 mongoose.connect(mongoURL, {
--- server.js
+++ server.js
@@ -7,22 +7,11 @@ app.use(bodyParser.json());
 const validator = require('./helpers/validate');
 //import the router
 const personRoutes = require('./routes/personRoutes');
+require('dotenv').config();
 //use the router
 app.use('/',personRoutes);
-require('dotenv').config();
+
 const PORT = process.env.PORT || 3000;
-
-// app.get('/getIdliDetails', function (req, res) {
-//   var customizedIdli = {
-//     "name"      : "Idli",
-//     "type"      : "Rawa Idli",
-//     "size"      : "10 CM", 
-//     "is_sambhar": true,
-//     "is_chutney": true
-//   }
-//   res.send(customizedIdli);
-// })
-
 
 app.listen(PORT, () => {
   console.log('Server is listening on port 3000')
